d7baf11ee204c85015d61563d698a016fd8737a0
tahzima <tahzima.i@gmail.com>
2023-04-14 09:41:35 +0000

    route de test

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function () {
+    return "hello sddi 4";
+});
 
 Route::middleware([
     'auth:sanctum',
