135a59917032e65baf1961c5ac85b4f70bfcc8b4
tahzima <tahzima.i@gmail.com>
2023-04-26 16:39:46 +0100

    add routes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,19 +4,28 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        DB::table('users')->insert([
+            'nom' => Str::random(10),
+            'prenom' => Str::random(10),
+            // 'dateNaissance' => 02/05/2023,
+            'role' => Str::random(10),
+            'adresse' => Str::random(10),
+            'genre' => Str::random(10),
+            'email' => Str::random(10).'@gmail.com',
+            'password' => Str::random(10),
+            'categorie_id' => 4
+        ]);
     }
 }
